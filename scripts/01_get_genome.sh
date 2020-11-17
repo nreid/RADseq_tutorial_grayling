93a5cb39b0752ca0dec28374d91b325e3ee27769
Noah Reid <noah.reid@uconn.edu>
2020-11-17 12:59:29 -0500

    get genome

---
+++ scripts/01_get_genome.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=get_genome
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -c 1
+#SBATCH --mem=128M
+#SBATCH --partition=general
+#SBATCH --qos=general
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=first.last@uconn.edu
+#SBATCH -o %x_%j.out
+#SBATCH -e %x_%j.err
+
+# download genome assembly for Arctic grayling, Thymallus thymallus
+# accession # GCA_004348285.1
+
+GENOMEDIR=../genome
+mkdir -p $GENOMEDIR
+
+wget \
+https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/004/348/285/GCA_004348285.1_ASM434828v1/GCA_004348285.1_ASM434828v1_genomic.fna.gz \
+../
+
+# index the genome using bwa
+module load bwa/0.7.17
+
+bwa index -p $GENOMEDIR/grayling $GENOMEDIR/GCA_004348285.1_ASM434828v1_genomic.fna.gz
--- scripts/runall.sh
+++ scripts/runall.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 jid1=$(sbatch --parsable 00_symlinkdata.sh )
-jid2=$(sbatch --parsable --dependency=afterok:$jid1 01_process_radtags.sh )
+jid2=$(sbatch --parsable --dependency=afterok:$jid1 01_get_genome.sh )
+jid3=$(sbatch --parsable --dependency=afterok:$jid2 02_process_radtags.sh )
