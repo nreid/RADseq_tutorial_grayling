eda38df0dcc6d092259a8d7c8dc7a90ca6c06af4
Noah Reid <noah.reid@uconn.edu>
2020-11-18 12:16:46 -0500

    align qc

---
+++ scripts/06c_alignQC.sh
@@ -0,0 +1,41 @@
+#!/bin/bash 
+#SBATCH --job-name=samstats
+#SBATCH --mail-user=
+#SBATCH --mail-type=ALL
+#SBATCH -o samstats/%x_%j.out
+#SBATCH -e samstats/%x_%j.err
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=5
+#SBATCH --mem=5G
+#SBATCH --qos=general
+#SBATCH --partition=general
+
+# calculate alignment statistic for each file
+
+module load samtools/1.10
+
+# input, output directories
+
+INDIR=../results/aligned
+OUTDIR=../results/align_stats
+mkdir -p $OUTDIR
+
+
+# samtools bam statistics
+for file in $(find $INDIR -name "*bam"); 
+do 
+SAM=$(basename $file .bam)
+samtools stats $file >$OUTDIR/${SAM}.stats
+echo $file;
+done
+
+
+# put the basic stats all in one file. 
+
+FILES=($(find $OUTDIR -name "*.stats" | sort))
+
+grep "^SN" ${FILES[0]} | cut -f 2 > $OUTDIR/SN.txt
+for file in ${FILES[@]}
+do paste $OUTDIR/SN.txt <(grep ^SN $file | cut -f 3) > $OUTDIR/SN2.txt && \
+	mv $OUTDIR/SN2.txt $OUTDIR/SN.txt
+done
