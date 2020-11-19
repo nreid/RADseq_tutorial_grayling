4d742f948594990f9556a0d725590c12eec3a0b1
Noah Reid <noah.reid@uconn.edu>
2020-11-18 19:58:55 -0500

    fb parallel

--- scripts/07_freebayes_parallel.sh
+++ scripts/07_freebayes_parallel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-#SBATCH --job-name=freebayes
+#SBATCH --job-name=freebayes_parallel
 #SBATCH --mail-user=
 #SBATCH --mail-type=ALL
 #SBATCH -o %x_%j.out
@@ -30,8 +30,12 @@ POPMAP=../meta/popmap_total.txt
 BAMLIST=$OUTDIR/bam.list
 cat $POPMAP | cut -f 1 | sed 's/$/.bam/' | sed  "s,^,$INDIR," >$BAMLIST
 
-./freebayes-parallel \
-	<(python fasta_generate_regions.py ${GEN}.fai 5000000) 30 \
+# these are freebayes scripts found in the same location as the executable
+FBP=/isg/shared/apps/freebayes/1.3.1/scripts/freebayes-parallel
+MAKEREGIONS=/isg/shared/apps/freebayes/1.3.1/scripts/fasta_generate_regions.py
+
+$FBP \
+	<(python $MAKEREGIONS ${GEN}.fai 5000000) 30 \
 	-f ${GEN} \
 	--bam-list $OUTDIR/bam.list \
 	-m 30 \
