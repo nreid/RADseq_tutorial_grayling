b7a8843afddaba87391c7d92f577fc439f9b9a37
Noah Reid <noah.reid@uconn.edu>
2020-11-20 14:16:38 -0500

    reformat

--- scripts/08_filter_vcfs.sh
+++ scripts/08_filter_vcfs.sh
@@ -31,47 +31,47 @@ DENOVO=../results/stacks/denovo
 REFMAP=../results/stacks/refmap
 FREEBA=../results/freebayes
 
-#############################
-# filter SITES by missingness
-#############################
+# #############################
+# # filter SITES by missingness
+# #############################
 
-# also remove multiallelic sites and indels
+# # also remove multiallelic sites and indels
 
-# stacks de novo-------------------------------
-vcftools --gzvcf $DENOVO/populations.snps.vcf \
-	--max-missing-count 116 --mac 3 --remove-indels --max-alleles 2 --min-alleles 2 \
-	--recode \
-	--stdout | \
-	bgzip >$OUTDIR/stacks_denovo.vcf.gz
+# # stacks de novo-------------------------------
+# vcftools --gzvcf $DENOVO/populations.snps.vcf \
+# 	--max-missing-count 116 --mac 3 --remove-indels --max-alleles 2 --min-alleles 2 \
+# 	--recode \
+# 	--stdout | \
+# 	bgzip >$OUTDIR/stacks_denovo.vcf.gz
 
-	# output missing individual report
-	vcftools --gzvcf $OUTDIR/stacks_denovo.vcf.gz --out $OUTDIR/stacks_denovo --missing-indv
+# 	# output missing individual report
+# 	vcftools --gzvcf $OUTDIR/stacks_denovo.vcf.gz --out $OUTDIR/stacks_denovo --missing-indv
 
-# stacks refmap-------------------------------
-vcftools --gzvcf $REFMAP/populations.snps.dict.vcf.gz \
-	--max-missing-count 116 --mac 3 --remove-indels --max-alleles 2 --min-alleles 2 \
-	--recode \
-	--stdout | \
-	bgzip >$OUTDIR/stacks_refmap.vcf.gz
+# # stacks refmap-------------------------------
+# vcftools --gzvcf $REFMAP/populations.snps.dict.vcf.gz \
+# 	--max-missing-count 116 --mac 3 --remove-indels --max-alleles 2 --min-alleles 2 \
+# 	--recode \
+# 	--stdout | \
+# 	bgzip >$OUTDIR/stacks_refmap.vcf.gz
 
-	# output missing individual report
-	vcftools --gzvcf $OUTDIR/stacks_refmap.vcf.gz --out $OUTDIR/stacks_refmap --missing-indv
+# 	# output missing individual report
+# 	vcftools --gzvcf $OUTDIR/stacks_refmap.vcf.gz --out $OUTDIR/stacks_refmap --missing-indv
 
-# freebayes------------------------------------
-	# - note also that:
-		# 1. bcftools norm normalizes variant representation
-		# 2. vcfallelic primitives breaks down haplotype alleles into constituent parts
-GEN=../genome/GCA_004348285.1_ASM434828v1_genomic.fna
-bcftools norm -f $GEN $FREEBA/fb_parallel.vcf.gz | \
-	vcfallelicprimitives --keep-info --keep-geno | \
-	vcfstreamsort | \
-	vcftools --vcf - \
-	--max-missing-count 116 --mac 3 --remove-indels --max-alleles 2 --min-alleles 2 \
-	--recode \
-	--stdout | bgzip >$OUTDIR/fb.vcf.gz
+# # freebayes------------------------------------
+# 	# - note also that:
+# 		# 1. bcftools norm normalizes variant representation
+# 		# 2. vcfallelic primitives breaks down haplotype alleles into constituent parts
+# GEN=../genome/GCA_004348285.1_ASM434828v1_genomic.fna
+# bcftools norm -f $GEN $FREEBA/fb_parallel.vcf.gz | \
+# 	vcfallelicprimitives --keep-info --keep-geno | \
+# 	vcfstreamsort | \
+# 	vcftools --vcf - \
+# 	--max-missing-count 116 --mac 3 --remove-indels --max-alleles 2 --min-alleles 2 \
+# 	--recode \
+# 	--stdout | bgzip >$OUTDIR/fb.vcf.gz
 
-	# output missing individual report
-	vcftools --gzvcf $OUTDIR/fb.vcf.gz --out $OUTDIR/fb --missing-indv
+# 	# output missing individual report
+# 	vcftools --gzvcf $OUTDIR/fb.vcf.gz --out $OUTDIR/fb --missing-indv
 
 ###################################
 # filter INDIVIDUALS by missingness
@@ -81,10 +81,13 @@ bcftools norm -f $GEN $FREEBA/fb_parallel.vcf.gz | \
 DROPSAMPLES=$(tail -n +2 $OUTDIR/fb.imiss | awk '$5 > .1' | cut -f 1 | tr "\n" "," | sed 's/,$//')
 
 # drop samples with high rates of missing data, also exclude low variant quality variants from freebayes
+
 # stacks denovo
 bcftools view -s ^$DROPSAMPLES $OUTDIR/stacks_denovo.vcf.gz | bgzip -c > $OUTDIR/denovo_final.vcf.gz
+
 # stacks refmap
 bcftools view -s ^$DROPSAMPLES $OUTDIR/stacks_refmap.vcf.gz | bgzip -c > $OUTDIR/refmap_final.vcf.gz
+
 # freebayes
 bcftools view -s ^$DROPSAMPLES $OUTDIR/fb.vcf.gz | bcftools filter -s LowQual -e '%QUAL<50' | bgzip -c > $OUTDIR/fb_final.vcf.gz
 
